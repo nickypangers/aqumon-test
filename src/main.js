c694db42c37675a42bb503bde045feb5981fa0c4
Nixon <nixon.p@mosaicdigi.com>
2021-11-23 19:46:30 +0800

    added axios

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from "./App.vue";
 import store from "./store";
 import "./index.css";
 import "echarts";
+import axios from "axios";
+
+const apiKey = "e56f67ccc4013f655992231b1330763d";
+
+axios.defaults.baseURL = "";
 
 const app = createApp(App);
 app.use(store);
